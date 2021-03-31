8563be1f814924b3e5190115a25682ddbf275e96
Ellysio <theo.roncalli@yahoo.fr>
2021-03-31 14:58:48 +0200

    Premiere version

---
+++ src/Node.java
@@ -0,0 +1,16 @@
+
+public class Node {
+
+    int key;// qui  va contenir la valeur du noeud
+    Node  left;
+    Node  right;
+
+    public Node (int key){
+
+        this.key = key;
+        this.left = null;
+        this.right = null;
+
+    }
+
+}
