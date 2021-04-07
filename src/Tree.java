7778e185dbf190a461ba7bdf8c0cd5ffeaed5366
projetsimulation <75625865+projetsimulation@users.noreply.github.com>
2021-04-07 11:40:17 +0200

    version 3

--- src/Tree.java
+++ src/Tree.java
@@ -51,6 +51,16 @@ public class Tree {
             this.next_sibling.suffix();
         }
     }
+    public void width(){
+
+        if (this.first_child != null) {
+            this.first_child.width();
+        }
+        System.out.print(this.key + " ");
+        if (this.next_sibling != null) {
+            this.next_sibling.width();
+        }
+    }
 
 
 
@@ -65,6 +75,7 @@ public class Tree {
         T.prefix();
         System.out.println();
         T.suffix();
+        T.width();
 
 
 
