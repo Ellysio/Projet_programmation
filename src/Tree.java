9264791ee123a91ebc1d056f558233a606771bb5
Ellysio <theo.roncalli@yahoo.fr>
2021-04-09 11:23:39 +0200

    Amélioration de la classe Tree

--- src/Tree.java
+++ src/Tree.java
@@ -31,6 +31,88 @@ public class Tree {
         }
     }
 
+    public void putFirstChild(Tree tree){
+        this.first_child = tree;
+    }
+
+    public void putFirstChildIfAbsent(Tree tree){
+        if (this.first_child == null){
+            this.putFirstChild(tree);
+        }
+    }
+
+    public void putNextSibling(Tree tree){
+        this.next_sibling = tree;
+    }
+
+    public void putSiblingIfAbsent(Tree tree){
+        if (this.next_sibling == null){
+            this.putNextSibling(tree);
+        }
+    }
+
+    public void replace(String key){
+        this.key = key;
+    }
+
+    public void replace(String oldKey, String newKey){
+        if (this.key == oldKey){
+            this.key = newKey;
+        }
+    }
+
+    public void deleteFirstChild(){
+        this.first_child = null;
+    }
+
+    public void deleteNextSibling(){
+        this.next_sibling = null;
+    }
+
+    /** @return The number of elements of the tree */
+    public int count(){
+        if (this.first_child == null && this.next_sibling == null){
+            return 1;
+        }
+        else if (this.first_child == null){
+            return (1 + this.next_sibling.count());
+        }
+        else if (this.next_sibling == null){
+            return (1 + this.first_child.count());
+        }
+        else {
+            return (1 + this.first_child.count() + this.next_sibling.count());
+        }
+    }
+
+    /** @return The height of the tree (levels number) */
+    public int height(){
+        if (this.first_child == null && this.next_sibling == null){
+            return 1;
+        }
+        else if (this.first_child == null){
+            return this.next_sibling.height();
+        }
+        else if (this.next_sibling == null){
+            return (1 + this.first_child.height());
+        }
+        else {
+            return (Math.max(1 + this.first_child.height(),this.next_sibling.height()));
+        }
+    }
+
+    public String getKey(){
+        return this.key;
+    }
+
+    public Tree getFirstChild(){
+        return this.first_child;
+    }
+
+    public Tree getRightChild(){
+        return this.next_sibling;
+    }
+
     public void prefix(){
         System.out.print(this.key + " ");
         if (this.first_child != null) {
@@ -60,7 +142,8 @@ public class Tree {
         T.prefix();
         System.out.println();
         T.suffix();
-
+        System.out.println("\n" + "Taille : " + T.count());
+        System.out.println("Profondeur : " + T.height());
 
 
     }
