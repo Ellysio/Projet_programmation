ee8b3b463fd4d187a023189a442e83edcff2cef4
projetsimulation <75625865+projetsimulation@users.noreply.github.com>
2021-04-22 02:00:54 +0200

    The first version of the class parser to extract  the matching and the sequence from stockholm file

--- src/Parser.java
+++ src/Parser.java
@@ -7,22 +7,36 @@ import java.io.IOException;
 public class Parser {
     public static void main (String[] args )
     {
-        File file =new File("C:\\Users\\ilhem\\IdeaProjects\\POO\\Projet_programmation\\src\\RF00005.stockholm.txt");
-        //BufferedReader readfile=null;
-        //String rows;
-        try
-        {   FileReader fileReader=new FileReader(file);
-            BufferedReader readfile= new BufferedReader(fileReader);
-            String line = readfile.readLine();
-            System.out.println(line);
-        }
-        catch (FileNotFoundException e )
-        {
-            System.err.printf("le fichier %s n'a pas été trouvé" , file.toString());
-        }catch(IOException e){
-            System.err.println("impossible  " + file.toString());
+        File file =new File("Tap your file path here ");
+        BufferedReader readfile=null;
+        try {
+            FileReader fileReader = new FileReader(file);
+            readfile = new BufferedReader(fileReader);
+            String line;
+            String matching=null;
+            String sequence=null;
+            while ((line = readfile.readLine()) != null) {
+                if (line.startsWith("#=GC SS_cons")){
+                    matching = line.substring(27) ;
+                    System.out.println(matching);
+                }else if(line.startsWith("#=GC RF")) {
+                    sequence=line.substring(27);
+                    System.out.println(sequence);
 
+                }
+            }
+
+        }catch (FileNotFoundException error )
+        {
+            System.out.println("File was not found! " + file.toString());
+        }catch(IOException error ){
+            System.out.println("File can not be closed!  " + file.toString());
+
+        }catch(NullPointerException error){
+            System.out.println("File can not be opened !"+ file.toString());
         }
+
+
 
 
     }
